b84846a9a6f81760fd0bdfd6de38b244230f080f
guglielmo-andrew <guglielmo.andrew@gmail.com>
2019-08-27 12:23:50 -0700

    updated powerint1

--- powerint1.c
+++ powerint1.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 struct Ans {
     int number;
     struct Ans *next;
 };
 
+//Prototypes
 void printList(struct Ans *n);
+bool inList(struct Ans *m, int x);
 
 void main (void) { 
 
@@ -22,23 +25,40 @@ void main (void) {
     {
         for (int i = 0; i < bound; i++) {
             for (int j = 0; j < bound; j++) {
+                // See if x^i + y^j = a number
                 if (pow(x,i) + pow(y,j) == a) {
-                    answer_ptr->number = a;
-                    answer_ptr->next = malloc(sizeof(struct Ans));
-                    answer_ptr = answer_ptr->next;
+                    // If true, see if number is already in thie list
+                    if (!inList(start_ptr, a)) {
+                        // If not, put the number in the list and create new link
+                        answer_ptr->number = a;
+                        answer_ptr->next = malloc(sizeof(struct Ans));
+                        answer_ptr = answer_ptr->next;
+                    }
                 }
             }
         }
     }
+    // Print out answers
     printf("Answers: ");
     printList(start_ptr);
     printf("\n");
 };
 
-void printList(struct Ans *n) 
-    { 
-        while (n->next != NULL) { 
-            printf(" %i ", n->number); 
-            n = n->next; 
-        } 
-    };
+// A function to print the linked list
+void printList(struct Ans *n) { 
+    while (n->next != NULL) { 
+        printf(" %i ", n->number); 
+        n = n->next; 
+    } 
+};
+
+// A function to see if a number is in the linked list already
+bool inList(struct Ans *m, int x) {
+    while (m->next != NULL) {
+        if (m->number == x) {
+            return true;
+        }
+        m = m-> next;
+    }
+    return false;
+};
